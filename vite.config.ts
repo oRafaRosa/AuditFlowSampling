6bba08b51426bd5a066d43bcd70a4af7127f4458
Rafael Rosa <rafa.rafaelrs97@gmail.com>
2025-08-12 11:54:52 -0300

    Fix paths for GitHub Pages + charset

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  base: "/AuditFlowSampling/", // nome do repo
-};
+export default defineConfig({
+  plugins: [react()],
+  base: '/AuditFlowSampling/', 
+})
